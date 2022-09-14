69fed1a7f7b7e4ef48f99eca3dd3e0bedb3ec082
TrafikApostel <56306440+TrafikApostel@users.noreply.github.com>
2022-09-14 13:38:15 +0300

    Update natusorangecli.sh

--- natusorangecli.sh
+++ natusorangecli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/uservrsk.sh 
+wget -P /home/ubuntu https://raw.githubusercontent.com/TrafikApostel/blob/main/uservrsk.sh
 chmod +x /home/ubuntu/uservrsk.sh
 sleep 6
 sudo /home/ubuntu/uservrsk.sh
