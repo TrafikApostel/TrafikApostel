2f6e02896c4d7aebbb41af1b547935bab1f294de
TrafikApostel <56306440+TrafikApostel@users.noreply.github.com>
2022-09-14 17:21:03 +0300

    Update usernamecli.sh

--- usernamecli.sh
+++ usernamecli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -P /home/ubuntu https://github.com/TrafikApostel/TrafikApostel/blob/main/uservrsk.sh
-chmod +x /home/ubuntu/uservrsk.sh
+sudo chmod +x /home/ubuntu/uservrsk.sh
 sleep 6
 sudo /home/ubuntu/uservrsk.sh
